a2d1ab01f5e7fed495e90b275e80fd4d8c1ffd9d
Mariia Trachuk <staraxion@gmail.com>
2023-11-27 15:45:00 +0200

    PR fix - updated controllers

--- src/controllers/genres.controller.ts
+++ src/controllers/genres.controller.ts
@@ -7,155 +7,146 @@ import initCustomError from '../utils/initCustomError';
 import { statusCodes, errorMessages } from '../utils/constants';
 
 class GenresController {
-  static getAllGenres(req: Request, res: Response, next: NextFunction): void {
-    GenreModel.find()
-      .sort({ name: 1 })
-      .then((genres) => {
-        if (genres.length) {
-          res.status(statusCodes.OK).json(genres);
-        } else {
-          initCustomError(
-            next,
-            statusCodes.NOT_FOUND,
-            errorMessages.genresListNotFound
-          );
-        }
-      })
-      .catch(() => {
+  static async getAllGenres(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const genres = await GenreModel.find().sort({ name: 1 });
+
+      if (genres.length) {
+        res.status(statusCodes.OK).json(genres);
+      } else {
         initCustomError(
           next,
           statusCodes.NOT_FOUND,
           errorMessages.genresListNotFound
         );
-      });
+      }
+    } catch (err) {
+      initCustomError(
+        next,
+        statusCodes.NOT_FOUND,
+        errorMessages.genresListNotFound
+      );
+    }
   }
 
-  static createGenre(req: Request, res: Response, next: NextFunction): void {
-    const body = req.body as GenreBody;
-    const name = body.name?.trim().toLowerCase();
-    GenreModel.findOne({ name })
-      .then((duplicate) => {
-        if (duplicate) {
-          initCustomError(
-            next,
-            statusCodes.CONFLICT,
-            errorMessages.genreConflict
-          );
-        } else {
-          new GenreModel(body)
-            .save()
-            .then((genre) => {
-              res.status(statusCodes.CREATED).json(genre);
-            })
-            .catch((err) => {
-              initCustomError(
-                next,
-                statusCodes.BAD_REQUEST,
-                err.message,
-                err.name
-              );
-            });
-        }
-      })
-      .catch((err) => {
-        next(err);
-      });
+  static async createGenre(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const body = req.body as GenreBody;
+      const name = body.name?.trim().toLowerCase();
+      const duplicate = await GenreModel.findOne({ name });
+
+      if (duplicate) {
+        initCustomError(
+          next,
+          statusCodes.CONFLICT,
+          errorMessages.genreConflict
+        );
+      } else {
+        const genre = await new GenreModel(body).save();
+        res.status(statusCodes.CREATED).json(genre);
+      }
+    } catch (err) {
+      initCustomError(next, statusCodes.BAD_REQUEST, err.message, err.name);
+    }
   }
 
-  static getGenreById(req: Request, res: Response, next: NextFunction): void {
-    const params = req.params as ReqParamsId;
-    const { id } = params;
-    GenreModel.findById(id)
-      .then((genre) => {
-        if (genre) {
-          res.status(statusCodes.OK).json(genre);
-        } else {
-          initCustomError(
-            next,
-            statusCodes.NOT_FOUND,
-            errorMessages.genreNotFound
-          );
-        }
-      })
-      .catch(() => {
+  static async getGenreById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const params = req.params as ReqParamsId;
+      const { id } = params;
+      const genre = await GenreModel.findById(id);
+
+      if (genre) {
+        res.status(statusCodes.OK).json(genre);
+      } else {
         initCustomError(
           next,
           statusCodes.NOT_FOUND,
           errorMessages.genreNotFound
         );
-      });
+      }
+    } catch (err) {
+      initCustomError(next, statusCodes.NOT_FOUND, errorMessages.genreNotFound);
+    }
   }
 
-  static updateGenre(req: Request, res: Response, next: NextFunction): void {
-    const params = req.params as ReqParamsId;
-    const { id } = params;
-    const body = req.body as GenreBody;
-    const name = body.name?.trim().toLowerCase();
-    GenreModel.findOne({ _id: { $ne: id }, name })
-      .then((duplicate) => {
-        if (duplicate) {
-          initCustomError(
-            next,
-            statusCodes.CONFLICT,
-            errorMessages.genreConflict
-          );
-        } else {
-          GenreModel.findByIdAndUpdate(id, body, {
+  static async updateGenre(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const params = req.params as ReqParamsId;
+      const { id } = params;
+      const body = req.body as GenreBody;
+      const name = body.name?.trim().toLowerCase();
+      const duplicate = await GenreModel.findOne({ _id: { $ne: id }, name });
+
+      if (duplicate) {
+        initCustomError(
+          next,
+          statusCodes.CONFLICT,
+          errorMessages.genreConflict
+        );
+      } else {
+        try {
+          const genre = await GenreModel.findByIdAndUpdate(id, body, {
             new: true,
             runValidators: true
-          })
-            .then((genre) => {
-              if (genre) {
-                res.status(statusCodes.OK).json(genre);
-              } else {
-                initCustomError(
-                  next,
-                  statusCodes.NOT_FOUND,
-                  errorMessages.genreNotFound
-                );
-              }
-            })
-            .catch((err) => {
-              initCustomError(
-                next,
-                statusCodes.BAD_REQUEST,
-                err.message,
-                err.name
-              );
-            });
+          });
+
+          if (genre) {
+            res.status(statusCodes.OK).json(genre);
+          } else {
+            initCustomError(
+              next,
+              statusCodes.NOT_FOUND,
+              errorMessages.genreNotFound
+            );
+          }
+        } catch (err) {
+          initCustomError(next, statusCodes.BAD_REQUEST, err.message, err.name);
         }
-      })
-      .catch(() => {
-        initCustomError(
-          next,
-          statusCodes.NOT_FOUND,
-          errorMessages.genreNotFound
-        );
-      });
+      }
+    } catch (err) {
+      initCustomError(next, statusCodes.NOT_FOUND, errorMessages.genreNotFound);
+    }
   }
 
-  static deleteGenre(req: Request, res: Response, next: NextFunction): void {
-    const params = req.params as ReqParamsId;
-    const { id } = params;
-    GenreModel.findByIdAndDelete(id)
-      .then((genre) => {
-        if (genre) {
-          res.status(statusCodes.NO_CONTENT).send();
-        } else {
-          initCustomError(
-            next,
-            statusCodes.NOT_FOUND,
-            errorMessages.genreNotFound
-          );
-        }
-      })
-      .catch(() => {
+  static async deleteGenre(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const params = req.params as ReqParamsId;
+      const { id } = params;
+      const genre = await GenreModel.findByIdAndDelete(id);
+
+      if (genre) {
+        res.status(statusCodes.NO_CONTENT).send();
+      } else {
         initCustomError(
           next,
           statusCodes.NOT_FOUND,
           errorMessages.genreNotFound
         );
-      });
+      }
+    } catch (err) {
+      initCustomError(next, statusCodes.NOT_FOUND, errorMessages.genreNotFound);
+    }
   }
 }
 
--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -7,193 +7,182 @@ import initCustomError from '../utils/initCustomError';
 import { statusCodes, errorMessages } from '../utils/constants';
 
 class MoviesController {
-  static getAllMovies(req: Request, res: Response, next: NextFunction): void {
-    MovieModel.find()
-      .sort({ releaseDate: -1 })
-      .then((movies) => {
-        if (movies.length) {
-          res.status(statusCodes.OK).json(movies);
-        } else {
-          initCustomError(
-            next,
-            statusCodes.NOT_FOUND,
-            errorMessages.moviesListNotFound
-          );
-        }
-      })
-      .catch(() => {
-        initCustomError(
-          next,
-          statusCodes.NOT_FOUND,
-          errorMessages.moviesListNotFound
-        );
-      });
-  }
-
-  static createMovie(req: Request, res: Response, next: NextFunction): void {
-    const body = req.body as MovieBody;
-    const title = body.title?.trim();
-    const releaseDate = body.releaseDate;
-    MovieModel.findOne({
-      title: new RegExp(`^${title}$`, 'i'),
-      releaseDate
-    })
-      .then((duplicate) => {
-        if (duplicate) {
-          initCustomError(
-            next,
-            statusCodes.CONFLICT,
-            errorMessages.movieConflict
-          );
-        } else {
-          new MovieModel(body)
-            .save()
-            .then((movie) => {
-              res.status(statusCodes.CREATED).json(movie);
-            })
-            .catch((err) => {
-              initCustomError(
-                next,
-                statusCodes.BAD_REQUEST,
-                err.message,
-                err.name
-              );
-            });
-        }
-      })
-      .catch((err) => {
-        next(err);
-      });
-  }
-
-  static getMovieById(req: Request, res: Response, next: NextFunction): void {
-    const params = req.params as ReqParamsId;
-    const { id } = params;
-    MovieModel.findById(id)
-      .then((movie) => {
-        if (movie) {
-          res.status(statusCodes.OK).json(movie);
-        } else {
-          initCustomError(
-            next,
-            statusCodes.NOT_FOUND,
-            errorMessages.movieNotFound
-          );
-        }
-      })
-      .catch(() => {
-        initCustomError(
-          next,
-          statusCodes.NOT_FOUND,
-          errorMessages.movieNotFound
-        );
-      });
-  }
-
-  static updateMovie(req: Request, res: Response, next: NextFunction): void {
-    const params = req.params as ReqParamsId;
-    const { id } = params;
-    const body = req.body as MovieBody;
-    const title = body.title?.trim();
-    const releaseDate = body.releaseDate;
-    MovieModel.findOne({
-      _id: { $ne: id },
-      title: new RegExp(`^${title}$`, 'i'),
-      releaseDate
-    })
-      .then((duplicate) => {
-        if (duplicate) {
-          initCustomError(
-            next,
-            statusCodes.CONFLICT,
-            errorMessages.movieConflict
-          );
-        } else {
-          MovieModel.findByIdAndUpdate(id, body, {
-            new: true,
-            runValidators: true
-          })
-            .then((movie) => {
-              if (movie) {
-                res.status(statusCodes.OK).json(movie);
-              } else {
-                initCustomError(
-                  next,
-                  statusCodes.NOT_FOUND,
-                  errorMessages.movieNotFound
-                );
-              }
-            })
-            .catch((err) => {
-              initCustomError(
-                next,
-                statusCodes.BAD_REQUEST,
-                err.message,
-                err.name
-              );
-            });
-        }
-      })
-      .catch(() => {
-        initCustomError(
-          next,
-          statusCodes.NOT_FOUND,
-          errorMessages.movieNotFound
-        );
-      });
-  }
-
-  static deleteMovie(req: Request, res: Response, next: NextFunction): void {
-    const params = req.params as ReqParamsId;
-    const { id } = params;
-    MovieModel.findByIdAndDelete(id)
-      .then((movie) => {
-        if (movie) {
-          res.status(statusCodes.NO_CONTENT).send();
-        } else {
-          initCustomError(
-            next,
-            statusCodes.NOT_FOUND,
-            errorMessages.movieNotFound
-          );
-        }
-      })
-      .catch(() => {
-        initCustomError(
-          next,
-          statusCodes.NOT_FOUND,
-          errorMessages.movieNotFound
-        );
-      });
-  }
-
-  static searchMoviesByGenre(
+  static async getAllMovies(
     req: Request,
     res: Response,
     next: NextFunction
-  ): void {
-    const params = req.params as ReqParamsGenreName;
-    const { genreName } = params;
-    MovieModel.find({ genre: genreName })
-      .sort({ releaseDate: -1 })
-      .then((movies) => {
-        if (movies.length) {
-          res.status(statusCodes.OK).json(movies);
-        } else {
-          initCustomError(
-            next,
-            statusCodes.NOT_FOUND,
-            errorMessages.moviesListNotFound
-          );
-        }
-      })
-      .catch(() => {
+  ): Promise<void> {
+    try {
+      const movies = await MovieModel.find().sort({ releaseDate: -1 });
+
+      if (movies.length) {
+        res.status(statusCodes.OK).json(movies);
+      } else {
         initCustomError(
           next,
           statusCodes.NOT_FOUND,
           errorMessages.moviesListNotFound
         );
+      }
+    } catch (err) {
+      initCustomError(
+        next,
+        statusCodes.NOT_FOUND,
+        errorMessages.moviesListNotFound
+      );
+    }
+  }
+
+  static async createMovie(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const body = req.body as MovieBody;
+      const title = new RegExp(`^${body.title?.trim()}$`, 'i');
+      const releaseDate = body.releaseDate;
+      const duplicate = await MovieModel.findOne({ title, releaseDate });
+
+      if (duplicate) {
+        initCustomError(
+          next,
+          statusCodes.CONFLICT,
+          errorMessages.movieConflict
+        );
+      } else {
+        const movie = await new MovieModel(body).save();
+        res.status(statusCodes.CREATED).json(movie);
+      }
+    } catch (err) {
+      initCustomError(next, statusCodes.BAD_REQUEST, err.message, err.name);
+    }
+  }
+
+  static async getMovieById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const params = req.params as ReqParamsId;
+      const { id } = params;
+      const movie = await MovieModel.findById(id);
+
+      if (movie) {
+        res.status(statusCodes.OK).json(movie);
+      } else {
+        initCustomError(
+          next,
+          statusCodes.NOT_FOUND,
+          errorMessages.movieNotFound
+        );
+      }
+    } catch (err) {
+      initCustomError(next, statusCodes.NOT_FOUND, errorMessages.movieNotFound);
+    }
+  }
+
+  static async updateMovie(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const params = req.params as ReqParamsId;
+      const { id } = params;
+      const body = req.body as MovieBody;
+      const title = new RegExp(`^${body.title?.trim()}$`, 'i');
+      const releaseDate = body.releaseDate;
+      const duplicate = await MovieModel.findOne({
+        _id: { $ne: id },
+        title,
+        releaseDate
       });
+
+      if (duplicate) {
+        initCustomError(
+          next,
+          statusCodes.CONFLICT,
+          errorMessages.movieConflict
+        );
+      } else {
+        try {
+          const movie = await MovieModel.findByIdAndUpdate(id, body, {
+            new: true,
+            runValidators: true
+          });
+
+          if (movie) {
+            res.status(statusCodes.OK).json(movie);
+          } else {
+            initCustomError(
+              next,
+              statusCodes.NOT_FOUND,
+              errorMessages.movieNotFound
+            );
+          }
+        } catch (err) {
+          initCustomError(next, statusCodes.BAD_REQUEST, err.message, err.name);
+        }
+      }
+    } catch (err) {
+      initCustomError(next, statusCodes.NOT_FOUND, errorMessages.movieNotFound);
+    }
+  }
+
+  static async deleteMovie(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const params = req.params as ReqParamsId;
+      const { id } = params;
+      const movie = await MovieModel.findByIdAndDelete(id);
+
+      if (movie) {
+        res.status(statusCodes.NO_CONTENT).send();
+      } else {
+        initCustomError(
+          next,
+          statusCodes.NOT_FOUND,
+          errorMessages.movieNotFound
+        );
+      }
+    } catch (err) {
+      initCustomError(next, statusCodes.NOT_FOUND, errorMessages.movieNotFound);
+    }
+  }
+
+  static async searchMoviesByGenre(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const params = req.params as ReqParamsGenreName;
+      const { genreName } = params;
+      const movies = await MovieModel.find({ genre: genreName }).sort({
+        releaseDate: -1
+      });
+
+      if (movies.length) {
+        res.status(statusCodes.OK).json(movies);
+      } else {
+        initCustomError(
+          next,
+          statusCodes.NOT_FOUND,
+          errorMessages.moviesListNotFound
+        );
+      }
+    } catch (err) {
+      initCustomError(
+        next,
+        statusCodes.NOT_FOUND,
+        errorMessages.moviesListNotFound
+      );
+    }
   }
 }
 
